018811c52d6a63c546242a7cf3350dbfd60400dc
Ahmed Shawun <faysalahmadshwun@gmail.com>
2024-07-10 00:03:50 +0600

    date section almost done

--- Digital_Clock.py
+++ Digital_Clock.py
@@ -48,11 +48,29 @@ lab_am_text.place(x=650,y=175,height=30,width=120)
 
 # ******************Date Started***********************
 
-lab_date = Label(clock,text='00',font=('Ga Maamli',60,'bold'), bg='#b50b41',fg='white') # for hour (class,)
-lab_date.place(x=35,y=35,height=120,width=120)
+lab_date = Label(clock,text='00',font=('Ga Maamli',60,'bold'), bg='#b50b41',fg='white') # for Date (class,)
+lab_date.place(x=35,y=350,height=120,width=120)
 
-lab_date_text = Label(clock,text='Date',font=('Ga Maamli',24,'bold'), bg='#b50b41',fg='white') # for hour (class,)
+lab_date_text = Label(clock,text='Date',font=('Ga Maamli',24,'bold'), bg='#b50b41',fg='white') # for Date (class,)
 lab_date_text.place(x=35,y=275,height=30,width=120)
+
+lab_mo = Label(clock,text='00',font=('Ga Maamli',60,'bold'), bg='#b50b41',fg='white') # for Month (class,)
+lab_mo.place(x=250,y=350,height=120,width=120)
+
+lab_mo_text = Label(clock,text='Month',font=('Ga Maamli',24,'bold'), bg='#b50b41',fg='white') # for Month (class,)
+lab_mo_text.place(x=250,y=275,height=30,width=120)
+
+lab_year = Label(clock,text='00',font=('Ga Maamli',60,'bold'), bg='#b50b41',fg='white') # for year (class,)
+lab_year.place(x=450,y=350,height=120,width=120)
+
+lab_year_text = Label(clock,text='Year',font=('Ga Maamli',24,'bold'), bg='#b50b41',fg='white') # for year (class,)
+lab_year_text.place(x=450,y=275,height=30,width=120)
+
+lab_day = Label(clock,text='00',font=('Ga Maamli',60,'bold'), bg='#b50b41',fg='white') # for Day (class,)
+lab_day.place(x=650,y=350,height=120,width=120)
+
+lab_day_text = Label(clock,text='Day',font=('Ga Maamli',24,'bold'), bg='#b50b41',fg='white') # for Day (class,)
+lab_day_text.place(x=650,y=275,height=30,width=120)
 
 
 
