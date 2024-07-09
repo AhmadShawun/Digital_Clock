01ca62887ae8ff58d18625e29f83af8744c383ac
Ahmed Shawun <faysalahmadshwun@gmail.com>
2024-07-09 16:11:06 +0600

    time section done

--- Digital_Clock.py
+++ Digital_Clock.py
@@ -1,4 +1,18 @@
 from tkinter import *
+import datetime
+#print(datetime.datetime.now())
+
+def dateTime():
+    time = datetime.datetime.now()
+    hour = time.strftime('%I')
+    minutes = time.strftime('%M')
+    second = time.strftime("%S")
+    am = time.strftime("%p")
+    lab_hr.config(text=hour)
+    lab_min.config(text=minutes)
+    lab_sec.config(text=second)
+    lab_am.config(text=am)
+    lab_hr.after(200,dateTime) # change provided graphics
 
 clock = Tk()
 clock.title('Digital Clock') # Title
@@ -6,29 +20,30 @@ clock.title('Digital Clock') # Title
 clock.geometry('800x600') #for window size by pixels
 clock.config(bg='#064534') # Background color
 
-lab_hr = Label(clock,text='00',font=('DM Led',60,'bold'), bg='#b50b41',fg='white') # for hour (class,)
+lab_hr = Label(clock,text='00',font=('Ga Maamli',60,'bold'), bg='#b50b41',fg='white') # for hour (class,)
 lab_hr.place(x=35,y=35,height=120,width=120)
 
-lab_hr_text = Label(clock,text='Hour',font=('DM Led',24,'bold'), bg='#b50b41',fg='white') # for hour (class,)
+lab_hr_text = Label(clock,text='Hour',font=('Ga Maamli',24,'bold'), bg='#b50b41',fg='white') # for hour (class,)
 lab_hr_text.place(x=35,y=175,height=30,width=120)
 
-lab_min = Label(clock,text='00',font=('DM Led',60,'bold'), bg='#b50b41',fg='white') # for min (class,)
+lab_min = Label(clock,text='00',font=('Ga Maamli',60,'bold'), bg='#b50b41',fg='white') # for min (class,)
 lab_min.place(x=250,y=35,height=120,width=120)
 
-lab_min_text = Label(clock,text='Minutes',font=('DM Led',24,'bold'), bg='#b50b41',fg='white') # for hour (class,)
+lab_min_text = Label(clock,text='Minutes',font=('Ga Maamli',24,'bold'), bg='#b50b41',fg='white') # for hour (class,)
 lab_min_text.place(x=250,y=175,height=30,width=120)
 
-lab_sac = Label(clock,text='00',font=('DM Led',60,'bold'), bg='#b50b41',fg='white') # for min (class,)
-lab_sac.place(x=450,y=35,height=120,width=120)
+lab_sec = Label(clock,text='00',font=('Ga Maamli',60,'bold'), bg='#b50b41',fg='white') # for min (class,)
+lab_sec.place(x=450,y=35,height=120,width=120)
 
-lab_sac_text = Label(clock,text='Sec',font=('DM Led',24,'bold'), bg='#b50b41',fg='white') # for hour (class,)
-lab_sac_text.place(x=450,y=175,height=30,width=120)
+lab_sec_text = Label(clock,text='Sec',font=('Ga Maamli',24,'bold'), bg='#b50b41',fg='white') # for hour (class,)
+lab_sec_text.place(x=450,y=175,height=30,width=120)
 
-lab_am = Label(clock,text='00',font=('DM Led',60,'bold'), bg='#b50b41',fg='white') # for min (class,)
+lab_am = Label(clock,text='00',font=('Ga Maamli',60,'bold'), bg='#b50b41',fg='white') # for min (class,)
 lab_am.place(x=650,y=35,height=120,width=120)
 
-lab_am_text = Label(clock,text='Am/Pm',font=('DM Led',24,'bold'), bg='#b50b41',fg='white') # for hour (class,)
+lab_am_text = Label(clock,text='Am/Pm',font=('Ga Maamli',24,'bold'), bg='#b50b41',fg='white',) # for hour (class,)
 lab_am_text.place(x=650,y=175,height=30,width=120)
 
+dateTime()
 
 clock.mainloop()
