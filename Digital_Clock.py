aac31dbd88e19814520d903b554f9ad5041b1b96
Ahmed Shawun <faysalahmadshwun@gmail.com>
2024-07-09 15:34:40 +0600

    create a clock block

--- Digital_Clock.py
+++ Digital_Clock.py
@@ -6,4 +6,10 @@ clock.title('Digital Clock') # Title
 clock.geometry('800x400') #for window size by pixels
 clock.config(bg='#064534') # Background color
 
+lab_hr = Label(clock,text='00',font=('DM Led',60,'bold'), bg='#b50b41',fg='white') # for hour (class,)
+lab_hr.place(x=40,y=40,height=110,width=100)
+
+
+
+
 clock.mainloop()
