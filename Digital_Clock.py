b0c1a9a9fd8308e1d12d2e331bec73ab9199b151
Ahmed Shawun <faysalahmadshwun@gmail.com>
2024-07-09 15:26:32 +0600

    bg added

--- Digital_Clock.py
+++ Digital_Clock.py
@@ -1,8 +1,9 @@
 from tkinter import *
 
 clock = Tk()
-clock.title('    ****** Digital Clock ******') # Title
+clock.title('Digital Clock') # Title
 # Graphics work
-clock.geometry() #for window size
+clock.geometry('800x400') #for window size by pixels
+clock.config(bg='#064534') # Background color
 
 clock.mainloop()
